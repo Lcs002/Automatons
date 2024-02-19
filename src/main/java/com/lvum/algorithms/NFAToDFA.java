91d6b8ab94b90638e1a02d95790216ca51c1c889
LucasVianna <59148393+Lcs002@users.noreply.github.com>
2024-02-20 00:21:41 +0100

    NFA to DFA ??fixed??

--- src/main/java/com/lvum/algorithms/NFAToDFA.java
+++ src/main/java/com/lvum/algorithms/NFAToDFA.java
@@ -9,31 +9,44 @@ public class NFAToDFA implements Algorithm {
 
     @Override
     public Automaton run(Automaton automaton) {
+        // The automaton resultant of the conversion
         Automaton result = new Automaton(automaton.getLanguage());
+        // List of Set of States we have already checked
         List<Set<String>> marked = new ArrayList<>();
+        // Queue of Set of States
         Queue<Set<String>> queue = new LinkedList<>();
 
+        // Add and Mark the first State
         queue.add(new HashSet<>(Collections.singletonList(automaton.getInitialState())));
         marked.add(new HashSet<>(Collections.singletonList(automaton.getInitialState())));
 
+        // While there is still some Set of States to check...
         while (!queue.isEmpty()) {
-            Set<String> currentStates = queue.poll();
-            for (String state : currentStates) {
-                for (Character entry : automaton.getLanguage()) {
-                    Set<String> nextState =
+            Set<String> superstate = queue.poll();
+            for (Character entry : automaton.getLanguage()) {
+                Set<String> nextSuperstate = new HashSet<>();
+                for (String state : superstate) {
+                    // Get next states of the current state with a certain entry
+                    nextSuperstate.addAll(
                             automaton.getTransitionsFrom(state).stream()
                                     .filter(x -> x.entry().equals(entry))
                                     .map(Automaton.Transition::to)
-                                    .collect(Collectors.toSet());
-                    if (!marked.contains(nextState)) {
-                        queue.add(nextState);
-                        marked.add(nextState);
-                    }
-                    result.addTransition(state, String.join("-", nextState), entry);
+                                    .collect(Collectors.toSet())
+                    );
                 }
+                // Check if we have already evaluated the next state
+                if (!marked.contains(nextSuperstate)) {
+                    queue.add(nextSuperstate);
+                    marked.add(nextSuperstate);
+                }
+
+                result.addTransition(
+                        String.join("-", superstate),
+                        String.join("-", nextSuperstate),
+                        entry
+                );
             }
         }
-
         return result;
     }
 }
