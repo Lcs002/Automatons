e49d75236487ab3e69f520e89aa821cc3d5494e2
LucasVianna <59148393+Lcs002@users.noreply.github.com>
2024-02-22 19:38:19 +0100

    Equivalency needs rework.

--- src/test/java/com/lvum/NFAToDFAEpsilonTest.java
+++ src/test/java/com/lvum/NFAToDFAEpsilonTest.java
@@ -3,6 +3,9 @@ package com.lvum;
 import com.lvum.algorithms.Equivalency;
 import com.lvum.algorithms.NFAToDFA;
 import com.lvum.algorithms.NFAToDFAEpsilon;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -12,13 +15,13 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class NFAToDFAEpsilonTest {
-    @Test
-    void sameLanguage() {
-        // Test 1
-        // The language of the automaton must not change after the conversion
-        Set<Character> language = new HashSet<>(Arrays.asList('0', '1'));
-        Automaton automaton = new Automaton(language);
+    private Set<Character> language;
+    private Automaton automaton;
 
+    @BeforeEach
+    void beforeEach() {
+        language = new HashSet<>(Arrays.asList('0', '1'));
+        automaton = new Automaton(language);
         automaton.addTransition("S1", "S2", '0');
         automaton.addTransition("S1", "S1", '0');
         automaton.addTransition("S1", "S1", '1');
@@ -26,9 +29,13 @@ public class NFAToDFAEpsilonTest {
         automaton.addTransition("S2", "S2", '1');
         automaton.setInitialState("S1");
         automaton.addFinalState("S1");
+    }
 
+    @Test
+    void sameLanguage() {
+        // Test 1
+        // The language of the automaton must not change after the conversion
         Automaton result = automaton.run(new NFAToDFAEpsilon());
-
         assertEquals(result.getLanguage(), language);
     }
 
@@ -36,17 +43,6 @@ public class NFAToDFAEpsilonTest {
     void deterministic() {
         // Test 2
         // The resulting automaton must not have any state with the same entry more than once
-        Set<Character> language = new HashSet<>(Arrays.asList('0', '1'));
-        Automaton automaton = new Automaton(language);
-
-        automaton.addTransition("S1", "S2", '0');
-        automaton.addTransition("S1", "S1", '0');
-        automaton.addTransition("S1", "S1", '1');
-        automaton.addTransition("S2", "S1", '0');
-        automaton.addTransition("S2", "S2", '1');
-        automaton.setInitialState("S1");
-        automaton.addFinalState("S1");
-
         Automaton result = automaton.run(new NFAToDFAEpsilon());
 
         for (Automaton.Transition transition : result.getTransitions()) {
@@ -61,19 +57,8 @@ public class NFAToDFAEpsilonTest {
     void equivalent() {
         // Test 3
         // The resulting automaton must be equivalent to the original one
-        Set<Character> language = new HashSet<>(Arrays.asList('0', '1'));
-        Automaton original = new Automaton(language);
-
-        original.addTransition("S1", "S2", '0');
-        original.addTransition("S1", "S1", '0');
-        original.addTransition("S1", "S1", '1');
-        original.addTransition("S2", "S1", '0');
-        original.addTransition("S2", "S2", '1');
-        original.setInitialState("S1");
-        original.addFinalState("S1");
-
-        Automaton result = original.run(new NFAToDFA());
-
-        assertTrue(original.run(new Equivalency(result)));
+        Automaton result = automaton.run(new NFAToDFA());
+        assertTrue(automaton.run(new Equivalency(result)));
     }
+
 }
