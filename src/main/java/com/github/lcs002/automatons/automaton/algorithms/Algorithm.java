fe6dface68c7cc1acff89a1a6413c2bcd10a5def
LucasVianna <59148393+Lcs002@users.noreply.github.com>
2024-03-05 09:08:58 +0100

    Update Algorithm.java

--- src/main/java/com/github/lcs002/automatons/automaton/algorithms/Algorithm.java
+++ src/main/java/com/github/lcs002/automatons/automaton/algorithms/Algorithm.java
@@ -3,7 +3,7 @@ package com.github.lcs002.automatons.automaton.algorithms;
 import com.github.lcs002.automatons.automaton.Automaton;
 
 /**
- * <h1>Algorithm</h1>
+ * <h2>Algorithm</h2>
  * <p>Interface that represents an algorithm.</p>
  * <p>Every algorithm must implement this interface, implementing the method <b>run</b>.</p>
  * @param <T> The result of the algorithm.
